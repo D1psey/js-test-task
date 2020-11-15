738c788eb6b582919d8ed1d30bb34f9f6cd9b7d9
Alexey <d1psey878@gmail.com>
2020-11-15 02:06:53 +0200

    add

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const config = require('./config.json')
 const express = require('express');
 const app = express();
-const path = require('path');
+// const path = require('path');
 const port = 3000;
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
